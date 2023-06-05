5733559caa19168afaf4a7a2696a31f87de1236a
Wasif <wasif.mustahid@brainstation-23.com>
2023-06-05 18:46:41 +0600

    recording added

--- src/app/feature/components/speaking-exam/speaking-exam.component.ts
+++ src/app/feature/components/speaking-exam/speaking-exam.component.ts
@@ -1,73 +1,72 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {DomSanitizer} from "@angular/platform-browser";
 
 declare var $: any;
 import * as RecordRTC from 'recordrtc';
+import {AudioRecordingService} from "../../services/audio-recording.service";
 @Component({
   selector: 'app-speaking-exam',
   templateUrl: './speaking-exam.component.html',
   styleUrls: ['./speaking-exam.component.css']
 })
-export class SpeakingExamComponent {
-  title = 'micRecorder';
-  recording = false;
-  //URL of Blob
-  url;
-  error;
-  private record: any;
-  constructor(private domSanitizer: DomSanitizer) {
-  }
-  sanitize(url: string) {
-    return this.domSanitizer.bypassSecurityTrustUrl(url);
-  }
-  /**
-   * Start recording.
-   */
-  initiateRecording() {
+export class SpeakingExamComponent implements OnDestroy{
+  isRecording = false;
+  recordedTime;
+  blobUrl;
+  teste;
 
-    this.recording = true;
-    let mediaConstraints = {
-      video: false,
-      audio: true
-    };
-    navigator.mediaDevices
-        .getUserMedia(mediaConstraints)
-        .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+  constructor(
+      private audioRecordingService: AudioRecordingService,
+      private sanitizer: DomSanitizer
+  ) {
+    this.audioRecordingService
+        .recordingFailed()
+        .subscribe(() => (this.isRecording = false));
+    this.audioRecordingService
+        .getRecordedTime()
+        .subscribe(time => (this.recordedTime = time));
+    this.audioRecordingService.getRecordedBlob().subscribe(data => {
+      this.teste = data;
+      this.blobUrl = this.sanitizer.bypassSecurityTrustUrl(
+          URL.createObjectURL(data.blob)
+      );
+    });
   }
-  /**
-   * Will be called automatically.
-   */
-  successCallback(stream) {
-    var options = {
-      mimeType: "audio/wav",
-      numberOfAudioChannels: 1,
-      sampleRate: 16000,
-    };
-    //Start Actuall Recording
-    var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
-    this.record = new StereoAudioRecorder(stream, options);
-    this.record.record();
+
+  startRecording() {
+    if (!this.isRecording) {
+      this.isRecording = true;
+      this.audioRecordingService.startRecording();
+    }
   }
-  /**
-   * Stop recording.
-   */
+
+  abortRecording() {
+    if (this.isRecording) {
+      this.isRecording = false;
+      this.audioRecordingService.abortRecording();
+    }
+  }
+
   stopRecording() {
-    this.recording = false;
-    this.record.stop(this.processRecording.bind(this));
+    if (this.isRecording) {
+      this.audioRecordingService.stopRecording();
+      this.isRecording = false;
+    }
   }
 
-  processRecording(blob) {
-    this.url = URL.createObjectURL(blob);
-    console.log("blob", blob);
-    console.log("url", this.url);
+  clearRecordedData() {
+    this.blobUrl = null;
   }
-  /**
-   * Process Error.
-   */
-  errorCallback(error) {
-    this.error = 'Can not play audio in your browser';
-  }
-  ngOnInit() {
 
+  ngOnDestroy(): void {
+    this.abortRecording();
+  }
+
+  download(): void {
+    const url = window.URL.createObjectURL(this.teste.blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = this.teste.title;
+    link.click();
   }
 }
