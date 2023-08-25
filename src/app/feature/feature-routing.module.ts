b3353c0775bead4df2d3b9a62ca2a727ae94722e
Ahmed Nusayer <108522022+AhmedNusayer86@users.noreply.github.com>
2023-08-25 16:50:03 +0600

    topic based exam

--- src/app/feature/feature-routing.module.ts
+++ src/app/feature/feature-routing.module.ts
@@ -8,6 +8,8 @@ import { AboutUsComponent } from "./components/about-us/about-us.component";
 import { ContactUsComponent } from "./components/contact-us/contact-us.component";
 import { ResultListComponent } from "./components/result-list/result-list.component";
 import {AdminComponent} from "./components/admin/admin.component";
+import {TopicCardComponent} from "./components/topic-card/topic-card.component";
+import {TopicBasedExamComponent} from "./components/topic-based-exam/topic-based-exam.components";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
@@ -18,6 +20,8 @@ const routes: Routes = [
   { path: "about-us", component: AboutUsComponent },
   { path: "contact-us", component: ContactUsComponent },
   { path: "admin", component: AdminComponent },
+  { path: "topic-card", component: TopicCardComponent},
+  { path: "topic-based-exam", component: TopicBasedExamComponent}
 ];
 
 @NgModule({
